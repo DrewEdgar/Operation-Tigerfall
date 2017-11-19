17932d7f9756b9466d9dfffe6a6562e5cc1b7539
Drew Edgar <drew.edgar4@gmail.com>
2017-11-19 15:25:58 -0500

    Sharing from home PC to old laptop

---
+++ Drew-share/My Block Model/Assets/Introducer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //My commenting discipline needs work
+        print("Wilee Darklight was here");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
